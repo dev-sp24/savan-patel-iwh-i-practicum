b4ac747ab935749d65b043514c6b3092fbbb0cb8
dev-sp24 <savan.thoughtmate@gmail.com>
2025-12-13 15:32:37 +0530

    Added Update-cobj Code

--- index.js
+++ index.js
@@ -13,11 +13,38 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/update-cobj", (req, res) => {
- 
+  res.render("update-cobj");
 });
 
 app.post("/update-cobj", async (req, res) => {
+  const { name,price,model } =
+    req.body;
+  let customObjectRecordId = null;
+  const data = {
+    properties: {
+      name,
+      price,
+      model,
+    },
+  };
 
+  const url = `https://api.hubspot.com/crm/v3/objects/2-222070893`;
+
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Custom object created:", response.data);
+    customObjectRecordId = response.data.id;
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error creating custom object:", error.message);
+    res.send("Error creating custom object");
+  }
 
 });
 
